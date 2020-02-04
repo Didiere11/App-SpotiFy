4367d51a361b94e2c5ba61296bf6567b22ed05aa
unknown <ramirezrenteriajosedidiere@gmail.com>
2020-02-04 15:18:56 -0600

    busquedas completas

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +7,17 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class SearchComponent {
+  artistas: any [] = [];
 
-  constructor() { }
+  constructor( private spotify: SpotifyService ) { }
 
 buscar(termino: string){
   console.log(termino);
+  this.spotify.getArtista( termino )
+  .subscribe( (data: any) => {
+    console.log(data);
+    this.artistas = data;
+  });
 }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,15 +9,34 @@ export class SpotifyService {
   constructor(private http: HttpClient) { 
 
   }
-  getNewReleases() {
-    const headers = new HttpHeaders ({
-      'Authorization': 'Bearer BQCS1wNIPY--PqDpZk834JfOvrLGxet4b2sTkltUS211zIIX86Yq8oF047bk1MbAlX9KFhdQ7sBMAWQv0IY'
-    });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+  getQuery( query: string){
+    const url = `https://api.spotify.com/v1/${ query }`;
+    const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQCq65wS0K2RdHEoq6V7SuVOrzpdFy-U8j9jeQ6ClQqTRoWhN8TEuqVmb2Ry1v9BHTpLO-BhEwWjDixhRBQ'
+    });
+    return this.http.get(url, {headers});
+  }
+
+  getNewReleases() {
+    /*const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQDf0zZ7kGcHbh_nFDCoK9yd6VAcnysKY1bNxZwfkAnC-x5tobQezyU20JyW0ZFsrkSJDQd7zrLGjaY7STM'
+    });
+*/  
+    return this.getQuery('browse/new-releases?limit=20')
+    .pipe( map (data =>  data['albums'].items));
+    /*return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+                    .pipe( map (data =>  data['albums'].items));*/
   }
   getArtista( termino: string ){
+    /*const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQDf0zZ7kGcHbh_nFDCoK9yd6VAcnysKY1bNxZwfkAnC-x5tobQezyU20JyW0ZFsrkSJDQd7zrLGjaY7STM'
+    });*/
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                      .pipe( map(data => data['artists'].items ));
 
+    /*return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+                      .pipe( map(data => data['artists'].items ));*/
   }
 
 }
